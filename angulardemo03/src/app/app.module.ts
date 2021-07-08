78852678d843e6f714bc082f9a89044caf5a57d5
jingchen01 <landscape.chen@gmail.com>
2021-07-08 06:21:29 -0700

    Add header, home components

--- angulardemo03/src/app/app.module.ts
+++ angulardemo03/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NewsComponent } from './components/news/news.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NewsComponent
+    NewsComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
